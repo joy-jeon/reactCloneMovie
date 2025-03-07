c412f26d95fb890aa3b5255c0525d1ef0198e9e5
joy-jeon <ha0913hyun@hanmail.net>
2025-03-07 13:50:17 +0900

    #6.0 Introduction - rerender problem

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("새로고침 되고있어요")
   return (
     <div>
-      <h1>welcome</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>버튼입니다.</button>
     </div>
   );
 }
