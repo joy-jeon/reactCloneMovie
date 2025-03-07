14804cdaf67d0cb8ee69c6880d8056b417177fb1
joy-jeon <ha0913hyun@hanmail.net>
2025-03-07 15:19:52 +0900

    #6.4 Cleanu - reset

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,16 +1,8 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter((prev) => prev + 1);
-  console.log("매번 새로고침")
-  useEffect(() => {
-    console.log("한번실행")
-  }, []);
   return (
     <div>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>버튼입니다.</button>
     </div>
   );
 }
