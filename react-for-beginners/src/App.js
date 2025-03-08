756db076a79cf0b222438850b726e6d60a075961
joy-jeon <ha0913hyun@hanmail.net>
2025-03-08 23:50:59 +0900

    #7.2 Coin Tracker - select

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -15,9 +15,13 @@ function App() {
     <div>
       <h1>The Coins! ({coins.length})</h1>
       {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((item) => <li>{item.name} {item.symbol} : ${item.quotes.USD.price} USD</li>)}
-      </ul>
+      <select>
+        {coins.map((item) => (
+          <option>
+            <li>{item.name} {item.symbol} : ${item.quotes.USD.price} USD</li>
+          </option>
+        ))}
+      </select>
     </div>
   )
 }
