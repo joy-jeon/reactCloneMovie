242182b65a4ef19fe20a2d1401276738d5a1e541
joy-jeon <ha0913hyun@hanmail.net>
2025-03-08 18:32:13 +0900

    #7.0 To Do List part One

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -3,8 +3,28 @@ import { useState, useEffect } from "react";
 
 
 function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === '') {
+      return
+    }
+    setToDos((curruentArray) => [toDo, ...curruentArray])
+    setToDo('');
+  }
+  console.log(toDos)
   return (
-    <div></div>
+    <form onSubmit={onSubmit}>
+      <h1>My To Do List ({toDos.length})</h1>
+      <input
+        value={toDo}
+        onChange={onChange}
+        type="text"
+      />
+      <button>버튼</button>
+    </form>
   )
 }
 
