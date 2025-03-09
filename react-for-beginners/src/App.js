a663df9aa675b89212584e21bb50b58c1cd56c6d
joy-jeon <ha0913hyun@hanmail.net>
2025-03-09 13:33:53 +0900

    #7.3 Movie App part On - async, await (1)

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -3,15 +3,27 @@ import { useState, useEffect } from 'react';
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+    )
+    const movie = await response.json();
+    setLoading(false)
+    setMovies(movie.data.movies)
+  }
   useEffect(() => {
-    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
-      .then((response) => response.json())
-      .then((movie) => {
-        setLoading(false)
-        setMovies(movie.data.movies)
-      })
+    getMovies()
   }, [])
-  console.log(movies)
+
+  // useEffect(() => {
+  //   fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
+  //     .then((response) => response.json())
+  //     .then((movie) => {
+  //       setLoading(false)
+  //       setMovies(movie.data.movies)
+  //     })
+  // }, [])
+
   return (
     <div>
       {loading ? <h3>로딩중 입니다.</h3> : null}
