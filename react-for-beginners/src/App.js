bfe903e6899a1dc2fddf66e03ff01f8036e0dc06
joy-jeon <ha0913hyun@hanmail.net>
2025-03-09 12:19:25 +0900

    #7.2 Coin Tracker - view condition

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -13,15 +13,24 @@ function App() {
   }, [])
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <select>
-        {coins.map((item) => (
-          <option>
-            <li>{item.name} {item.symbol} : ${item.quotes.USD.price} USD</li>
-          </option>
-        ))}
-      </select>
+      {/* 로딩중일 때 개수 숨기기, 로딩 끝나면 개수 보여주기 */}
+      {/* as-is ({coins.length}) , to-be `(${coins.length})` */}
+      <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+      {/* 로딩중일 때 select 숨기기, 로딩 끝나면 select 보여주기 */}
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((item) => (
+            <option>
+              <li>{item.name} {item.symbol} : ${item.quotes.USD.price} USD</li>
+            </option>
+          ))}
+        </select>
+      )
+      }
+
+
     </div>
   )
 }
