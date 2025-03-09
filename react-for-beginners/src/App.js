38fb6b93f8f6434c91367f3a929f45863906c469
joy-jeon <ha0913hyun@hanmail.net>
2025-03-09 15:24:37 +0900

    #7.5 React Router

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,45 +1,18 @@
-import { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8&sort_by=year"
-      )).json();
-
-    setLoading(false)
-    setMovies(json.data.movies)
-  }
-  useEffect(() => {
-    getMovies()
-  }, [])
-
-  console.log(movies)
-
-  return (
-    <div>
-      {loading ? (
-        <h3>로딩중 입니다.</h3>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} alt="" />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <Router>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
 }
 
 export default App;
