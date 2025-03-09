5c6239d1e98b67dffb9eb88e2a0906896b2913f2
joy-jeon <ha0913hyun@hanmail.net>
2025-03-09 13:22:22 +0900

    #7.3 Movie App part On - fetch

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,6 +1,20 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
+      .then((response) => response.json())
+      .then((movie) => {
+        setLoading(false)
+        setMovies(movie.data.movies)
+      })
+  }, [])
+  console.log(movies)
   return (
     <div>
+      {loading ? <h3>로딩중 입니다.</h3> : null}
     </div>
   )
 }
